dd670443e20efe4c6c29cf7bb787691da013ce80
Amirhameed <aamirjalali8@gmail.com>
2023-11-20 20:16:22 -0500

    Initial Commit 2

---
+++ Plugins/ALS-Refactored-4.13/Intermediate/Build/Win64/UnrealEditor/Inc/ALS/UHT/AlsControlRigInput.generated.h
@@ -0,0 +1,28 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "State/AlsControlRigInput.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ALS_AlsControlRigInput_generated_h
+#error "AlsControlRigInput.generated.h already included, missing '#pragma once' in AlsControlRigInput.h"
+#endif
+#define ALS_AlsControlRigInput_generated_h
+
+#define FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALS_Public_State_AlsControlRigInput_h_8_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FAlsControlRigInput_Statics; \
+	static class UScriptStruct* StaticStruct();
+
+
+template<> ALS_API UScriptStruct* StaticStruct<struct FAlsControlRigInput>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALS_Public_State_AlsControlRigInput_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Plugins/ALS-Refactored-4.13/Intermediate/Build/Win64/UnrealEditor/Inc/ALSEditor/UHT/AlsAnimGraphNode_CurvesBlend.generated.h
@@ -0,0 +1,66 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Nodes/AlsAnimGraphNode_CurvesBlend.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ALSEDITOR_AlsAnimGraphNode_CurvesBlend_generated_h
+#error "AlsAnimGraphNode_CurvesBlend.generated.h already included, missing '#pragma once' in AlsAnimGraphNode_CurvesBlend.h"
+#endif
+#define ALSEDITOR_AlsAnimGraphNode_CurvesBlend_generated_h
+
+#define FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_SPARSE_DATA
+#define FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_ACCESSORS
+#define FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUAlsAnimGraphNode_CurvesBlend(); \
+	friend struct Z_Construct_UClass_UAlsAnimGraphNode_CurvesBlend_Statics; \
+public: \
+	DECLARE_CLASS(UAlsAnimGraphNode_CurvesBlend, UAnimGraphNode_Base, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/ALSEditor"), NO_API) \
+	DECLARE_SERIALIZER(UAlsAnimGraphNode_CurvesBlend)
+
+
+#define FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UAlsAnimGraphNode_CurvesBlend(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UAlsAnimGraphNode_CurvesBlend(UAlsAnimGraphNode_CurvesBlend&&); \
+	NO_API UAlsAnimGraphNode_CurvesBlend(const UAlsAnimGraphNode_CurvesBlend&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UAlsAnimGraphNode_CurvesBlend); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UAlsAnimGraphNode_CurvesBlend); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UAlsAnimGraphNode_CurvesBlend) \
+	NO_API virtual ~UAlsAnimGraphNode_CurvesBlend();
+
+
+#define FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_7_PROLOG
+#define FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_SPARSE_DATA \
+	FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_ACCESSORS \
+	FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_INCLASS_NO_PURE_DECLS \
+	FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h_10_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ALSEDITOR_API UClass* StaticClass<class UAlsAnimGraphNode_CurvesBlend>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_StealthProject_Plugins_ALS_Refactored_4_13_Source_ALSEditor_Public_Nodes_AlsAnimGraphNode_CurvesBlend_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
